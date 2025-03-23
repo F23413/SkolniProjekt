8b7238f559d7875ab9214dfcde56dc6d0121f465
Natalie_Sukova <natalie.sukova@gmail.com>
2025-03-23 21:52:10 +0100

    Projekt Start

---
+++ src/main/java/com/kvadrazicdev/KvadRazic/KvadRazicApplication.java
@@ -0,0 +1,13 @@
+package com.kvadrazicdev.KvadRazic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvadRazicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvadRazicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kvadrazicdev/KvadRazic/KvadRazicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kvadrazicdev.KvadRazic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvadRazicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
