682bd9be29e662f749418ae92da85f41c2638892
Natalie_Sukova <natalie.sukova@gmail.com>
2025-04-06 11:47:17 +0200

    Entita Uzivatel

---
+++ src/main/java/com/kvadrazicdev/KvadRazic/entity/Uzivatel.java
@@ -0,0 +1,64 @@
+package com.kvadrazicdev.KvadRazic.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "uzivatele")
+public class Uzivatel implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @NotBlank(message = "Vyplňte prosím pole Jméno")
+    private String Jmeno;
+    @NotBlank(message = "Vyplňte prosím pole Email")
+    @Column(unique = true)
+    private String Email;
+    @NotBlank(message = "Vyplňte prosím pole Telefonní číslo")
+    private String TelCislo;
+    private String Heslo;
+    private String Role;
+    private List<Pujcka> Pujcky = new ArrayList<>();
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(Role));
+    }
+
+    @Override
+    public String getUsername() {
+        return Email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
