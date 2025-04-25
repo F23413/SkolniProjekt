a8811d2d2b08de8f8694e8cf54b9ef70be250575
Natalie_Sukova <natalie.sukova@gmail.com>
2025-04-25 15:07:23 +0200

    tvorba navbaru

---
+++ src/component/common/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import ApiService from "../../service/ApiService";
+
+function Navbar(){
+    const isAuthenticated = ApiService.isAuthenticated();
+    const isAdmin = ApiService.isAdmin();
+    const isUzivatel = ApiService.isUzivatel();
+    const navigate = useNavigate();
+
+    const handleLogout =()=>{
+        const isLogout = window.confirm("Chcete se odhlásit?");
+        if(isLogout){
+            ApiService.logout();
+            navigate('/home')
+        }
+    }
+
+    return(
+        <nav className="navbar">
+            <div className="navbar-znacka">
+                <NavLink to="/home">Kvadrazic</NavLink>
+            </div>
+            <ul className="navbar-ul">
+                <li><NavLink to="/home" activeClass="active">Domů</NavLink></li>
+                <li><NavLink to="/filmy" activeClass="active">Filmy</NavLink></li>
+                <li><NavLink to="/pujcene-filmy" activeClass="active">Moje půjčky</NavLink></li>
+
+                { isUzivatel && <li><NavLink to="/profil" activeClass="active">Můj profil</NavLink></li>}
+                { isAdmin && <li><NavLink to="/admin" activeClass="active">Admin</NavLink></li>}
+
+                {!isAuthenticated && <li><NavLink to="/login" activeClass="active">Přihlásit se</NavLink></li>}
+                {!isAuthenticated && <li><NavLink to="/registrace" activeClass="active">Registrace</NavLink></li>}
+
+                {!isAuthenticated && <li onClick={handleLogout}>Odhlásit se</li>}
+
+            </ul>
+        </nav>
+    )
+
+}
+
+export default Navbar;
