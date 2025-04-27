69a1725b834aad41d3786625f00e8cc14c1e4cc0
Natalie_Sukova <natalie.sukova@gmail.com>
2025-04-27 16:51:40 +0200

    stranka registrace

---
+++ src/component/auth/RegistracePage.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import ApiService from '../../service/ApiService';
+import { useNavigate } from 'react-router-dom';
+
+function RegistracePage() {
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        jmeno: '',
+        email: '',
+        heslo: '',
+        telCislo: ''
+    });
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        const { jmeno, value } = e.target;
+        setFormData({ ...formData, [jmeno]: value });
+    };
+
+    const validateForm = () => {
+        const { jmeno, email, heslo, telCislo } = formData;
+        if (!jmeno || !email || !heslo || !telCislo) {
+            return false;
+        }
+        return true;
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!validateForm()) {
+            setErrorMessage('Vyplňte prosím všechna pole.');
+            setTimeout(() => setErrorMessage(''), 5000);
+            return;
+        }
+        try {
+            // Call the register method from ApiService
+            const response = await ApiService.registrUzivatel(formData);
+
+            // Check if the response is successful
+            if (response.kodStavu === 200) {
+                // Clear the form fields after successful registration
+                setFormData({
+                    jmeno: '',
+                    email: '',
+                    heslo: '',
+                    telCislo: ''
+                });
+                setSuccessMessage('Registrace proběhla úspěšně');
+                setTimeout(() => {
+                    setSuccessMessage('');
+                    navigate('/');
+                }, 3000);
+            }
+        }
+         catch (error) {
+            setErrorMessage(error.response?.data?.message || error.message);
+            setTimeout(() => setErrorMessage(''), 5000);
+        }
+    };
+
+    return (
+        <div className="auth-container">
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        {successMessage && <p className="success-message">{successMessage}</p>}
+            <h2>Sign Up</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Jméno:</label>
+                    <input type="text" name="jmeno" value={formData.jmeno} onChange={handleInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label>Email:</label>
+                    <input type="email" name="email" value={formData.email} onChange={handleInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label>Phone Number:</label>
+                    <input type="text" name="telCislo" value={formData.telCislo} onChange={handleInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label>heslo:</label>
+                    <input type="password" name="heslo" value={formData.heslo} onChange={handleInputChange} required />
+                </div>
+                <button type="submit">Zaregistrovat se</button>
+            </form>
+            <p className="register-link">
+                Máte již účet? <a href="/login">Login</a>
+            </p>
+        </div>
+    );
+}
+
+export default RegistracePage;
